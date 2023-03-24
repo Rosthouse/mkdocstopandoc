dfc81ec88c7bda37de3306eb18ff8e4061ef4215
rosthouse <rosthouse@gmail.com>
2023-03-24 08:36:16 +0100

    Removing wrongly placed tasks.json Adding entry point in setup.py, so that it can be found by pandoc Adding documentation

---
+++ readme.md
@@ -0,0 +1,21 @@
+# MKDocs to Pandoc
+
+This provides several filters:
+
+- [Content Tabs](https://squidfunk.github.io/mkdocs-material/reference/content-tabs/)
+
+  Add this to your metadata:
+
+  ```yaml
+  content-tabs: [""]
+  ```
+
+  Each of the listed content tabs will be rendered in the final output, in sequence.
+
+- [Admonitions](https://squidfunk.github.io/mkdocs-material/reference/admonitions/)
+
+  Admonitions will be converted to blockquotes
+
+- [Icons](https://squidfunk.github.io/mkdocs-material/reference/icons-emojis/)
+
+  Converts icons to inline images
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="MKDocsToPandoc",                     # This is the name of the package
+    name="mkdocstopandoc",                  # This is the name of the package
     version="0.0.1",                        # The initial release version
     author="Rosthouse",                     # Full name of the author
     description="A panflute filter for pandoc, which converts the specific markdown extensions of MKDocs",
@@ -19,8 +19,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.8',                # Minimum version requirement of the package
-    py_modules=["MKDocsToPandoc"],             # Name of the python package
+    py_modules=["mkdocstopandoc"],             # Name of the python package
     # Directory of the source code of the package
+    entry_points={'console_scripts': ['mkdocstopandoc = mkdocstopandoc:main']},
     package_dir={'': 'src'},
     install_requires=[
         "panflute>=2.3.0"
